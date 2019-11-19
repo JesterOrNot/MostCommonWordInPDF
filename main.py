315fc6b56e6f1fb68cdfdfcb66c57bde3cdafeb3
Sean Hellum <seanhellum45@gmail.com>
2019-11-19 20:04:27 +0000

    dne

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
+import textract
+
+
+# Reads the text of a pdf into a string then returns said string
 def read_pdf_to_string(file_name):
-    pass
+    return textract.process(file_name)
 
-
+# Splits words into a list
 def string_to_list(string: str):
-    word_list = string.split(" ")
-    return word_list
+    return string.split(" ")
 
+# Fiters punctuation out of word list
 def filter_list(the_list: list):
     final_list = []
-    punctuation = [",", ";", "(", ")", "[", "]", "{", "}", '"', "'", ".", "”"]
+    punctuation = [",", ";", "(", ")", "[", "]",
+                   "{", "}", '"', "'", ".", "”", "\n", "“"]
     for i in the_list:
+        if len(i) == 0:
+            continue
         filtered_word = ""
-        split_str = i.split()
+        split_str = list(i)
         for l in split_str:
             if l in punctuation:
-                pass
+                continue
             else:
                 filtered_word += l
-        final_list += filtered_word
+        final_list.append(filtered_word)
+    return final_list
 
-
+# Counts num of times word occurs in list and adds them to a dict
 def create_dict_from_list(the_list: list):
-    pass
+    my_dict = {}
+    for i in the_list:
+        if not i in my_dict.keys():
+            my_dict[i] = 0
+        else:
+            my_dict[i] = my_dict.get(i)+1
+    return my_dict
 
-
+# Iterates through a dict and finds the largest value and returns it's key
 def find_most_frequent_word(word_dict: dict):
-    pass
+    largest = word_dict[0]
+    for key, value in word_dict.items():
+        if value > word_dict.get(largest):
+            largest = key
+    return largest
 
 
 if __name__ == "__main__":
-    pass
+    print(find_most_frequent_word(create_dict_from_list(filter_list(string_to_list(read_pdf_to_string("Salvage the Bones ( PDFDrive.com ).pdf"))))))
--- test.py
+++ test.py
@@ -13,7 +13,30 @@ def test_string_to_list():
             “Ain’t nothing hit us in years."""
     z = main.string_to_list(text)
     assert z == text.split(" "), "Wrong!"
-
-
+def test_filter_list():
+    text = """and listening to our feet slide and push across floorboards.
+            Junior had a good arm, and bottles and cans rolled out from under the house
+            like pool balls. They stopped when they hit the rusted-over cow bath Daddy had
+            salvaged from the junkyard where he scraps metal. He’d brought it home for
+            Junior’s birthday last year and told him to use it as a swimming pool.
+            “Shoot,” Randall said. He was sitting on a chair under his homemade
+            basketball goal, a rim he’d stolen from the county park and screwed into the
+            trunk of a dead pine tree.
+            “Ain’t nothing hit us in years."""
+    the_list = main.string_to_list(text)
+    # print(the_list)
+    filtered_list = main.filter_list(the_list)
+    # print(filtered_list)
 if __name__ == "__main__":
-    test_string_to_list()
+    # test_string_to_list()
+    # test_filter_list()
+    text = """and listening to our feet slide and push across floorboards.
+            Junior had a good arm, and bottles and cans rolled out from under the house
+            like pool balls. They stopped when they hit the rusted-over cow bath Daddy had
+            salvaged from the junkyard where he scraps metal. He’d brought it home for
+            Junior’s birthday last year and told him to use it as a swimming pool.
+            “Shoot,” Randall said. He was sitting on a chair under his homemade
+            basketball goal, a rim he’d stolen from the county park and screwed into the
+            trunk of a dead pine tree.
+            “Ain’t nothing hit us in years."""
+    print(main.create_dict_from_list(main.filter_list(main.string_to_list(text))))
